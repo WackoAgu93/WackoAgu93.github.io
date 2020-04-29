bd98c6017e409c380d10579470cec4d6c4e9d7f1
WackoAgu93 <60129824+WackoAgu93@users.noreply.github.com>
2020-04-29 11:24:47 +0900

    20200429

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -19,7 +19,53 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            $.ajax({
+
+
+            let nodemailer = require('nodemailer');
+            let transporter = nodemailer.createTransport({
+                service:'gmail',
+                auth: {
+                    user : 'agumail93@gmail.com',
+                    pass : 'qwer12345!'
+                }
+            });
+
+            let mailOption = {
+                from : email,
+                to : 'agumail93@gmail.com',
+                subject : 'nodemailer test',
+                text : '이름 : '+name+'<br>'+'핸드폰 번호'+phone+'<br>'+'이메일 : '+email+'<br>'+message
+            };
+
+            transporter.sendMail(mailOption, function(err, info) {
+                if ( err ) {
+                    console.error('Send Mail error : ', err);
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+                else {
+                    console.log('Message sent : ', info);
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                }
+            });
+
+/*            $.ajax({
                 url: "/mail/contactformhandler.php",
                 type: "POST",
                 data: {
@@ -53,7 +99,7 @@ $(function() {
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-            });
+            });*/
         },
         filter: function() {
             return $(this).is(":visible");
